8d9a7619974fb3122ef422f16b37e00a99d21e20
masinde70 <5812481+masinde70@users.noreply.github.com>
2025-01-28 11:39:49 +0200

    projct configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1440px',
+    },
+    extend: {
+      colors: {
+        strongCyan: 'hs(171, 66%, 44%)',
+        lightBlue: 'hs(233, 100%, 69%)',
+        darkGrayishBlue: 'hs(210, 10%, 33%)',
+        grayishBlue: 'hs(201, 11%, 66%)',
+        },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
